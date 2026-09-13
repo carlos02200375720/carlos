d8943e94794b50b468fbf1d4674534c8c3207ca3
carlos <cg0220037@gmail.com>
2026-09-13 10:27:52 -0400

    refactor(server): export all mongoose models from model index

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -2,3 +2,5 @@ export { MongoUser } from "./User";
 export { MongoPublicacion } from "./Publicacion";
 export { MongoProduct } from "./Product";
 export { MongoReel } from "./Reel";
+export { MongoCart } from "./Cart";
+export { MongoOrder } from "./Order";
